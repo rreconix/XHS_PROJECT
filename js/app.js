181b27181446ef7b366933abf7d0b9267e2da1f3
felixhansen1 <felix.hansen1007@gmail.com>
2022-06-11 22:40:24 -0400

    master

--- js/app.js
+++ js/app.js
@@ -1,54 +1,53 @@
-const video = document.querySelector('#video')
-const videoRect = video.getBoundingClientRect()
+const video = document.querySelector('#video');
+const videoRect = video.getBoundingClientRect();
 
 video.addEventListener('click', e => {
-    console.log(e.clientX - videoRect.left, e.clientY - videoRect.top)
+    console.log(e.clientX - videoRect.left, e.clientY - videoRect.top);
 })
 
-const videoHeight = videoRect.height
-const videoWidth = videoRect.width
+const videoHeight = videoRect.height;
+const videoWidth = videoRect.width;
 
-const changeImg = async(img, sources, delay) => {
+const changeImg = (img, sources, delay) => {
     let counter = 0;
     setInterval(() => {
         img.src = sources[1 - counter % 2];
-        counter++
+        counter++;
     }, delay)
 }
 
-export const placeImage = async({ x, y, w = 75, h = 75, source1, source2 = undefined, delay = 1000 } = {}) => {
+export const placeImage = ({ x, y, w = 75, h = 75, source1, source2 = undefined, delay = 1000 } = {}) => {
     if([x,y, source1].some(val => val == undefined)){
         return console.warn('Invalid parameters');
     }
     const img = new Image(w, h);
-    img.src = source1
-    img.style.left = x + '%'
-    img.style.top = y + '%'
-    video.appendChild(img)
+    img.src = source1;
+    img.style.left = x + '%';
+    img.style.top = y + '%';
+    video.appendChild(img);
 
     if(source2 !== undefined){
-        await changeImg(img, [source1, source2], delay)
+        changeImg(img, [source1, source2], delay);
     }
 }
 
 export const enterMultipleImages = (array) => {
     array.forEach(obj => {
-        placeImage(obj)
+        placeImage(obj);
     })
 }
-
 
 export const sleep = async(ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const removeChildren = async(index1, index2 = video.childElementCount - index1) => {
-    let children = []
-    for(let i = 0; i < index2; i ++){
-        children.push([...video.children][index1 + i])
+export const removeChildren = (index, deleteCount = video.childElementCount - index) => {
+    let children = [];
+    for(let i = 0; i < deleteCount; i ++){
+        children.push([...video.children][index + i]);
     }
 
     children.forEach(child => {
-        video.removeChild(child)
+        video.removeChild(child);
     })
 }
--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 import { text } from '../text.js'
 import { placeImage, sleep, enterMultipleImages, removeChildren } from './app.js'
+
 enterMultipleImages(text[0])
+await sleep(1000)
 enterMultipleImages(text[1])
-await removeChildren(0)
+await sleep(1000)
 placeImage(
     {x: 60, y: 70, w: 200, h: 200, source1: './images/blastoise.png'}
 )
-await sleep(10000)
-
+await sleep(1000)
+removeChildren(0)
