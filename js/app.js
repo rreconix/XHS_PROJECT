bf83bfcc38c26552512e2a285da7a5f5d6a31b5f
felixhansen1 <felix.hansen1007@gmail.com>
2022-06-16 20:57:34 -0400

    final

--- js/app.js
+++ js/app.js
@@ -5,48 +5,39 @@ video.addEventListener('click', e => {
     console.log(e.clientX - videoRect.left, e.clientY - videoRect.top);
 })
 
-const changeImg = (img, sources, delay) => {
-    let counter = 0;
-    setInterval(() => {
-        img.src = sources[1 - counter % 2];
-        counter++;
-    }, delay)
-}
-
-const placeImage = ({ x, y, w = 10, h = 10, source1, source2 = undefined, speed = 1000 } = {}) => {
-    source2 = undefined
-    if([x,y, source1].some(val => val == undefined)) throw new Error('Invalid parameters');
-    if([x, y, h, w].some(val => val < 0 || val >= 100)) throw new Error('Value out of range')
+const placeImage = (image) => {
     const img = new Image();
-    img.src = source1;
-    img.style.width = w + '%';
-    img.style.left = x + '%';
-    img.style.top = y + '%';
+    img.src = image;
     video.appendChild(img);
-
-    if(source2 !== undefined){
-        changeImg(img, [source1, source2], speed);
-    }
 }
 
-export const sleep = async(ms) => {
+const sleep = async(ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-export const placeImages = async(array) => {
-    for(const obj of array){
-        await sleep(obj.delay)
-        placeImage(obj)
+export const placeImages = async(images) => {
+    placeImage(images[0])
+    for(const img of images){
+        placeImage(img)
+        await sleep(200)
+        removeChild()
     }
+    await sleep(3000)
+    placeImages(images)
 }
 
-export const removeChildren = (index, deleteCount = video.childElementCount - index) => {
-    let children = [];
-    for(let i = 0; i < deleteCount; i ++){
-        children.push([...video.children][index + i]);
+const removeChild = () => {
+    const child = [...video.children][0] || undefined;
+    if(child) video.removeChild(child)
+}
+
+export const getImageSources = () => {
+    let images = [];
+    for(let i = 1660; i < 1734; i++){
+        if(i !== 1723){
+            images.push(`./images/IMG-${i}.jpg`)
+        }
     }
 
-    children.forEach(child => {
-        video.removeChild(child);
-    })
+    return images
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-import { text } from '../text.js'
-import { sleep, placeImages, removeChildren } from './app.js'
+import { placeImages, getImageSources } from './app.js'
 
-placeImages(text[0])
-await sleep(1000)
-placeImages(text[1])
-placeImages(text[2])
-await sleep(3000)
-//removeChildren(0)
+placeImages(getImageSources())
